987d2b88041fdbf5372aee422d421bb6ce0222ff
Jorge Aparicio <jorge@japaric.io>
2017-07-06 23:22:42 -0500

    port firmware to the rtfm! macro
    cc japaric/cortex-m-rtfm#34

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,32 +1,29 @@
 #![deny(warnings)]
 #![feature(const_fn)]
+#![feature(plugin)]
 #![feature(used)]
 #![no_std]
+#![plugin(rtfm_macros)]
 
 extern crate blue_pill;
 extern crate cast;
 extern crate cortex_m_rt;
 #[macro_use]
 extern crate cortex_m_rtfm as rtfm;
-extern crate embedded_hal as hal;
 extern crate nb;
 extern crate shared;
-
-use core::cell::Cell;
 
 use blue_pill::capture::Event;
 use blue_pill::dma::{Buffer, Dma1Channel4, Dma1Channel5};
 use blue_pill::gpio::{self, PB12, PB13, PB14, PB15};
 use blue_pill::led::{self, Green};
+use blue_pill::prelude::*;
 use blue_pill::stm32f103xx;
 use blue_pill::time::{Hertz, Microseconds};
-use blue_pill::timer::Channel;
-use blue_pill::{Capture, Pwm, Serial, Timer};
+use blue_pill::{Capture, Channel, Pwm, Serial, Timer};
 use cast::i32;
-use hal::prelude::*;
-use rtfm::{C1, Local, P0, P1, Resource, T0, T1, TMax};
+use rtfm::Threshold;
 use shared::{Command, State};
-use stm32f103xx::interrupt::{DMA1_CHANNEL4, DMA1_CHANNEL5, TIM1_UP_TIM10, TIM4};
 
 // CONFIGURATION
 const BAUD_RATE: Hertz = Hertz(115_200);
@@ -37,123 +34,133 @@ const TIMER_FREQUENCY: Hertz = Hertz(1);
 const NERF_NUM: i32 = 3;
 const NERF_DEN: i32 = 4;
 
-// RESOURCES
-peripherals!(stm32f103xx, {
-    AFIO: Peripheral { ceiling: C0, },
-    DMA1: Peripheral { ceiling: C1, },
-    DWT: Peripheral { ceiling: C0, },
-    GPIOA: Peripheral { ceiling: C0, },
-    GPIOB: Peripheral { ceiling: C0, },
-    GPIOC: Peripheral { ceiling: C0, },
-    RCC: Peripheral { ceiling: C0, },
-    TIM1: Peripheral { ceiling: C1, },
-    TIM3: Peripheral { ceiling: C1, },
-    TIM4: Peripheral { ceiling: C1, },
-    USART1: Peripheral { ceiling: C1, },
-});
+// TASKS AND RESOURCES
+rtfm! {
+    device: blue_pill::stm32f103xx,
 
-static ACTIVE: Resource<Cell<bool>, C1> = Resource::new(Cell::new(false));
-static DUTY_L: Resource<Cell<i16>, C1> = Resource::new(Cell::new(0));
-static DUTY_R: Resource<Cell<i16>, C1> = Resource::new(Cell::new(0));
-static POS_L: Resource<Cell<u8>, C1> = Resource::new(Cell::new(0));
-static POS_R: Resource<Cell<u8>, C1> = Resource::new(Cell::new(0));
-static RX_BUFFER: Resource<Buffer<[u8; 3], Dma1Channel5>, C1> =
-    Resource::new(Buffer::new([0; 3]));
-static TX_BUFFER: Resource<Buffer<[u8; 11], Dma1Channel4>, C1> =
-    Resource::new(Buffer::new([0; 11]));
-static SLEEP_CYCLES: Resource<Cell<u32>, C1> = Resource::new(Cell::new(0));
+    resources: {
+        ACTIVE: bool = false;
+        DUTY_L: i16 = 0;
+        DUTY_R: i16 = 0;
+        POS_L: u8 = 0;
+        POS_R: u8 = 0;
+        RX_BUFFER: Buffer<[u8; 3], Dma1Channel5> = Buffer::new([0; 3]);
+        SLEEP_CYCLES: u32 = 0;
+        TX_BUFFER: Buffer<[u8; 11], Dma1Channel4> = Buffer::new([0; 11]);
+    },
+
+    init: {
+        path: init,
+    },
+
+    idle: {
+        path: idle,
+        resources: [DWT, SLEEP_CYCLES],
+    },
+
+    tasks: {
+        DMA1_CHANNEL4: {
+            enabled: true,
+            priority: 1,
+            resources: [DMA1, TX_BUFFER],
+        },
+
+        DMA1_CHANNEL5: {
+            enabled: true,
+            priority: 1,
+            resources: [
+                ACTIVE,
+                DMA1,
+                DUTY_L,
+                DUTY_R,
+                RX_BUFFER,
+                TIM3,
+                USART1,
+            ],
+        },
+
+        TIM1_UP_TIM10: {
+            enabled: true,
+            priority: 1,
+            resources: [
+                ACTIVE,
+                DMA1,
+                DUTY_L,
+                DUTY_R,
+                POS_L,
+                POS_R,
+                SLEEP_CYCLES,
+                TIM1,
+                TX_BUFFER,
+                USART1,
+            ],
+        },
+
+        TIM4: {
+            enabled: true,
+            priority: 1,
+            resources: [POS_L, POS_R, TIM4],
+        },
+    },
+}
 
 // INITIALIZATION
-fn init(ref prio: P0, thr: &TMax) {
-    let afio = &AFIO.access(prio, thr);
-    let dma1 = &DMA1.access(prio, thr);
-    let dwt = &DWT.access(prio, thr);
-    let gpioa = &GPIOA.access(prio, thr);
-    let gpiob = &GPIOB.access(prio, thr);
-    let gpioc = &GPIOC.access(prio, thr);
-    let rcc = &RCC.access(prio, thr);
-    let rx_buffer = RX_BUFFER.access(prio, thr);
-    let tim1 = TIM1.access(prio, thr);
-    let tim3 = TIM3.access(prio, thr);
-    let tim4 = TIM4.access(prio, thr);
-    let usart1 = USART1.access(prio, thr);
+fn init(p: init::Peripherals, r: init::Resources) {
+    let capture = Capture(p.TIM4);
+    let pwm = Pwm(p.TIM3);
+    let serial = Serial(p.USART1);
+    let timer = Timer(p.TIM1);
 
-    let capture = Capture(&*tim4);
-    let pwm = Pwm(&*tim3);
-    let serial = Serial(&*usart1);
-    let timer = Timer(&*tim1);
-
-    capture.init(CAPTURE_RESOLUTION, afio, gpiob, rcc);
-    dwt.enable_cycle_counter();
-    gpio::init(gpiob, rcc);
-    led::init(gpioc, rcc);
-    pwm.init(PWM_FREQUENCY.invert(), afio, gpioa, rcc);
-    serial.init(BAUD_RATE.invert(), afio, Some(dma1), gpioa, rcc);
-    timer.init(TIMER_FREQUENCY.invert(), rcc);
+    capture.init(CAPTURE_RESOLUTION, p.AFIO, p.GPIOB, p.RCC);
+    p.DWT.enable_cycle_counter();
+    gpio::init(p.GPIOB, p.RCC);
+    led::init(p.GPIOC, p.RCC);
+    pwm.init(PWM_FREQUENCY.invert(), p.AFIO, None, p.GPIOA, p.RCC);
+    serial.init(BAUD_RATE.invert(), p.AFIO, Some(p.DMA1), p.GPIOA, p.RCC);
+    timer.init(TIMER_FREQUENCY.invert(), p.RCC);
 
     capture.enable(Channel::_1);
     capture.enable(Channel::_2);
     capture.listen(Event::Capture1);
     capture.listen(Event::Capture2);
 
-    serial.read_exact(dma1, rx_buffer).unwrap();
+    serial.read_exact(p.DMA1, r.RX_BUFFER).unwrap();
     timer.resume();
 }
 
 // IDLE LOOP: CPU MONITOR
-fn idle(ref prio: P0, _thr: T0) -> ! {
+fn idle(_t: Threshold, mut r: idle::Resources) -> ! {
     loop {
-        rtfm::atomic(|thr| {
-            let dwt = DWT.access(prio, thr);
-            let sleep_cycles = SLEEP_CYCLES.access(prio, thr);
+        rtfm::atomic(|cs| {
+            let sleep_cycles = r.SLEEP_CYCLES.borrow_mut(cs);
 
-            let before = dwt.cyccnt.read();
+            let before = r.DWT.cyccnt.read();
             rtfm::wfi();
-            let after = dwt.cyccnt.read();
+            let after = r.DWT.cyccnt.read();
 
             let elapsed = after.wrapping_sub(before);
-            sleep_cycles.set(sleep_cycles.get() + elapsed);
+            **sleep_cycles += elapsed;
         });
     }
 }
 
 // TASKS
-tasks!(stm32f103xx, {
-    capture: Task {
-        interrupt: TIM4,
-        priority: P1,
-        enabled: true,
-    },
-    log: Task {
-        interrupt: TIM1_UP_TIM10,
-        priority: P1,
-        enabled: true,
-    },
-    rx: Task {
-        interrupt: DMA1_CHANNEL5,
-        priority: P1,
-        enabled: true,
-    },
-    tx: Task {
-        interrupt: DMA1_CHANNEL4,
-        priority: P1,
-        enabled: true,
-    },
+task!(DMA1_CHANNEL5, rx, Local {
+    x: i16 = 0;
+    y: i16 = 0;
 });
 
-/// Measure motor speed
-fn capture(_task: TIM4, ref prio: P1, ref thr: T1) {
-    let pos_l = POS_L.access(prio, thr);
-    let pos_r = POS_R.access(prio, thr);
-    let tim4 = TIM4.access(prio, thr);
+// Measure motor speed
+task!(TIM4, capture);
 
-    let capture = Capture(&*tim4);
+fn capture(_t: Threshold, r: TIM4::Resources) {
+    let capture = Capture(r.TIM4);
 
     match capture.capture(Channel::_1) {
         Err(nb::Error::Other(_)) => panic!(), // overcapture
         Err(nb::Error::WouldBlock) => {}
         Ok(_) => {
-            pos_l.set(pos_l.get() + 1);
+            **r.POS_L += 1;
         }
     }
 
@@ -161,84 +168,31 @@ fn capture(_task: TIM4, ref prio: P1, ref thr: T1) {
         Err(nb::Error::Other(_)) => panic!(), // overcapture
         Err(nb::Error::WouldBlock) => {}
         Ok(_) => {
-            pos_r.set(pos_r.get() + 1);
+            **r.POS_R += 1;
         }
     }
 }
 
-/// Log state periodically
-fn log(_task: TIM1_UP_TIM10, ref prio: P1, ref thr: T1) {
-    let active = ACTIVE.access(prio, thr);
-    let buffer = TX_BUFFER.access(prio, thr);
-    let dma1 = &DMA1.access(prio, thr);
-    let duty_l = DUTY_L.access(prio, thr);
-    let duty_r = DUTY_R.access(prio, thr);
-    let pos_l = POS_L.access(prio, thr);
-    let pos_r = POS_R.access(prio, thr);
-    let sleep_cycles = SLEEP_CYCLES.access(prio, thr);
-    let tim1 = TIM1.access(prio, thr);
-    let usart1 = USART1.access(prio, thr);
+// New command arrived
+fn rx(_t: Threshold, l: &mut Local, r: DMA1_CHANNEL5::Resources) {
+    let serial = Serial(r.USART1);
+    let pwm = Pwm(r.TIM3);
 
-    let timer = Timer(&*tim1);
-    let serial = Serial(&*usart1);
+    r.RX_BUFFER.release(r.DMA1).unwrap();
 
-    // clear the update flag
-    timer.wait().unwrap();
-
-    if active.get() {
-        let state = State {
-            duty_left: duty_l.get(),
-            duty_right: duty_r.get(),
-            sleep_cycles: sleep_cycles.get(),
-            speed_left: pos_l.get(),
-            speed_right: pos_r.get(),
-        };
-        state.serialize(&mut *buffer.borrow_mut());
-
-        serial.write_all(dma1, buffer).unwrap();
-    }
-
-    pos_l.set(0);
-    pos_r.set(0);
-    sleep_cycles.set(0);
-}
-
-/// New command arrived
-fn rx(ref task: DMA1_CHANNEL5, ref prio: P1, ref thr: T1) {
-    static X: Local<Cell<i16>, DMA1_CHANNEL5> = Local::new(Cell::new(0));
-    static Y: Local<Cell<i16>, DMA1_CHANNEL5> = Local::new(Cell::new(0));
-
-    let active = ACTIVE.access(prio, thr);
-    let buffer = RX_BUFFER.access(prio, thr);
-    let dma1 = &DMA1.access(prio, thr);
-    let duty_l = DUTY_L.access(prio, thr);
-    let duty_r = DUTY_R.access(prio, thr);
-    let tim3 = TIM3.access(prio, thr);
-    let usart1 = USART1.access(prio, thr);
-    let x = X.borrow(task);
-    let y = Y.borrow(task);
-
-    let serial = Serial(&*usart1);
-    let pwm = Pwm(&*tim3);
-
-    buffer.release(dma1).unwrap();
-
-    let cmd = {
-        let buffer = buffer.borrow();
-        Command::deserialize(&buffer)
-    };
+    let cmd = Command::deserialize(&r.RX_BUFFER.borrow());
 
     // queue a new read
-    serial.read_exact(dma1, buffer).unwrap();
+    serial.read_exact(r.DMA1, r.RX_BUFFER).unwrap();
 
     match cmd {
         Ok(Command::Start) => {
-            if active.get() {
+            if **r.ACTIVE {
                 Green.off();
 
-                active.set(false);
-                x.set(0);
-                y.set(0);
+                **r.ACTIVE = false;
+                l.x = 0;
+                l.y = 0;
 
                 // Hand brake
                 LeftMotor.brake();
@@ -252,19 +206,19 @@ fn rx(ref task: DMA1_CHANNEL5, ref prio: P1, ref thr: T1) {
                 pwm.enable(Channel::_1);
                 pwm.enable(Channel::_2);
 
-                active.set(true);
+                **r.ACTIVE = true;
             }
 
             return;
         }
-        Ok(Command::X(v)) if active.get() => x.set(v),
-        Ok(Command::Y(v)) if active.get() => y.set(v),
+        Ok(Command::X(v)) if **r.ACTIVE => l.x = v,
+        Ok(Command::Y(v)) if **r.ACTIVE => l.y = v,
         _ => return,
     }
 
-    if active.get() {
+    if **r.ACTIVE {
         // NOTE y < 0 means forwards, x > 0 means turn right
-        let (x, y) = (i32(x.get()), i32(y.get()));
+        let (x, y) = (i32(l.x), i32(l.y));
 
         // NOTE speed > 0 means forwards
         let speed = ((i32(pwm.get_max_duty()) * -y / NERF_DEN) * NERF_NUM) >>
@@ -281,25 +235,51 @@ fn rx(ref task: DMA1_CHANNEL5, ref prio: P1, ref thr: T1) {
         } else {
             LeftMotor.backward()
         }
-        duty_l.set(speed_l as i16);
-        LeftMotor.duty(&tim3, speed_l.abs() as u16);
+        **r.DUTY_L = speed_l as i16;
+        LeftMotor.duty(r.TIM3, speed_l.abs() as u16);
 
         if speed_r > 0 {
             RightMotor.forward()
         } else {
             RightMotor.backward()
         }
-        duty_r.set(speed_r as i16);
-        RightMotor.duty(&tim3, speed_r.abs() as u16);
+        **r.DUTY_R = speed_r as i16;
+        RightMotor.duty(r.TIM3, speed_r.abs() as u16);
     }
 }
 
-/// DMA transfer finished
-fn tx(_task: DMA1_CHANNEL4, ref prio: P1, ref thr: T1) {
-    let buffer = TX_BUFFER.access(prio, thr);
-    let dma1 = &DMA1.access(prio, thr);
+task!(TIM1_UP_TIM10, log);
 
-    buffer.release(dma1).unwrap();
+// Log state periodically
+fn log(_t: Threshold, r: TIM1_UP_TIM10::Resources) {
+    let timer = Timer(r.TIM1);
+    let serial = Serial(r.USART1);
+
+    // clear the update flag
+    timer.wait().unwrap();
+
+    if **r.ACTIVE {
+        let state = State {
+            duty_left: **r.DUTY_L,
+            duty_right: **r.DUTY_R,
+            sleep_cycles: **r.SLEEP_CYCLES,
+            speed_left: **r.POS_L,
+            speed_right: **r.POS_R,
+        };
+        state.serialize(&mut *r.TX_BUFFER.borrow_mut());
+
+        serial.write_all(r.DMA1, r.TX_BUFFER).unwrap();
+    }
+
+    **r.POS_L = 0;
+    **r.POS_R = 0;
+    **r.SLEEP_CYCLES = 0;
+}
+
+task!(DMA1_CHANNEL4, tx_transfer_done);
+
+fn tx_transfer_done(_t: Threshold, r: DMA1_CHANNEL4::Resources) {
+    r.TX_BUFFER.release(r.DMA1).unwrap();
 }
 
 // AUXILIARY
